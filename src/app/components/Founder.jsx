929a25c905dd710921934198fdfda1cc217c5efa
AnilVarma-G <ristoglobaltech.md@gmail.com>
2024-06-13 16:17:45 +0530

    Update Founder.jsx

--- src/app/components/Founder.jsx
+++ src/app/components/Founder.jsx
@@ -5,7 +5,7 @@ const Founder = () => {
     const images = [
         { src: '/chairperson.jpg', name: 'Dr. Sudha Rani, Chairperson' },
         { src: '/chairperson2.jpg', name: 'Dr. S Sameer Al-Hussaini, Academic Director' },
-        { src: '/chairperson3.jpg', name: 'Dr. MD Misbah Uddin, Managing Director'' }
+        { src: '/chairperson3.jpg', name: 'Dr. MD Misbah Uddin, Managing Director' }
     ];
 
     return (
